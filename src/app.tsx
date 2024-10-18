77fcb01fb04bcf5169e411e53c1b77dcb25a25ee
QuangDo <do.quang03@gmail.com>
2024-10-18 22:46:32 +0700

    Ez-1: Test ticket jira

--- src/app.tsx
+++ src/app.tsx
@@ -12,6 +12,7 @@ export function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
+        test ticket ez -1
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
